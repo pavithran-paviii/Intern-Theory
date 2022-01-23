2db2d0d61a44720ae6c1194eee262a7dd647ad0a
Anowar Hossain <anowar3042@gmail.com>
2022-01-23 14:43:40 +0530

    some updated

--- ek/index.js
+++ ek/index.js
@@ -1,29 +1,25 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 app.use(express.json());
-function connect(){
-    return mongoose.connect('mongodb://127.0.0.1:27017/intern')
-   
+function connect() {
+  return mongoose.connect("mongodb://127.0.0.1:27017/intern");
 }
-
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs"); // root directory for views views/
 app.use(express.static("public"));
 
-
-  const itSchema = new mongoose.Schema(
+const itSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     descp: { type: String, required: false },
     emi: { type: String, required: true },
     old_price: { type: Number, required: true },
-    new_price: { type: Number, required: true},
-    img_source:{ type: String, required: true},
-    btn_option: { type:String, required: true },
-
+    new_price: { type: Number, required: true },
+    img_source: { type: String, required: true },
+    btn_option: { type: String, required: true },
   },
   {
     versionKey: false,
@@ -31,13 +27,12 @@ app.use(express.static("public"));
   }
 );
 
-const It = mongoose.model("it", itSchema); 
+const It = mongoose.model("products", itSchema);
 
-app.get('/index', async(req,res) => {
+app.get("/index", async (req, res) => {
   try {
-     const items = await It.find().lean().exec();
- console.log(items)
-//      return res.render("index");
+    const items = await It.find().lean().exec();
+    //console.log(items)
     return res.render("index", {
       items: items,
     });
@@ -46,7 +41,6 @@ app.get('/index', async(req,res) => {
   }
 });
 
-
 const internSchema = new mongoose.Schema(
   {
     text1: { type: String, required: true },
@@ -54,9 +48,8 @@ const internSchema = new mongoose.Schema(
     text3: { type: String, required: true },
     test4: { type: String, required: false },
     text5: { type: String, required: true },
-    img:{ type: String, required: true},
-   apply: { type:String, required: true },
-
+    img: { type: String, required: true },
+    apply: { type: String, required: true },
   },
   {
     versionKey: false,
@@ -66,13 +59,11 @@ const internSchema = new mongoose.Schema(
 
 const Intern = mongoose.model("intrns", internSchema);
 
-
-
-app.get('/intern', async(req,res) => {
+app.get("/intern", async (req, res) => {
   try {
-     const items = await Intern.find().lean().exec();
- console.log(items)
-//      return res.render("index");
+    const items = await Intern.find().lean().exec();
+    console.log(items);
+    //      return res.render("index");
     return res.render("intern", {
       items: items,
     });
@@ -81,13 +72,13 @@ app.get('/intern', async(req,res) => {
   }
 });
 
-app.get('/', function(req, res){ 
-  res.render('index',{user:"John Smith"}) 
-// pass main ejs file name, pass any data variables 
-}); 
+app.get("/", function (req, res) {
+  res.render("index", { user: "John Smith" });
+  // pass main ejs file name, pass any data variables
+});
 
-app.listen(2345,async()=>{
-    await connect();
-    
-    console.log("server is running");
-})
+app.listen(2345, async () => {
+  await connect();
+
+  console.log("server is running");
+});
